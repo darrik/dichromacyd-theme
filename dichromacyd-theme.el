2b0f3297ab65af0ab20c538670c9763ea89f39ed
Rikard Glans <rikard@brainlag.se>
2012-05-26 16:18:51 +0200

    Add readme.

--- dichromacyd-theme.el
+++ dichromacyd-theme.el
@@ -5,7 +5,7 @@
 ;; Author: Chong Yidong <cyd@stupidchicken>
 ;; Modifications: Rikard Glans (rikard@brainlag.se)
 
-;; This file is part of GNU Emacs.
+;; This file is NOT part of GNU Emacs.
 
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
